0a84b8c63484d7988fb36c912c5ffb6b5d98fad0
Kamen Sharlandjiev <awskamen@amazon.com>
2021-04-01 12:56:06 +0100

    adding basing components and Amplify Auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Amplify, { API, Auth } from "aws-amplify";
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import aws_exports from "./aws-exports";
+import HeaderMenu from "./components/HeaderMenu";
+import OrderList from "./components/OrderList";
+import MyOrders from "./components/MyOrders";
+
+Amplify.configure(aws_exports);
+
+const APINAME = "deliveryAPI";
+let path = "/deliver";
+
+class App extends Component {
+  async componentDidMount() {
+    API.get(APINAME, path, {
+      headers: { "X-Amz-Security-Token": `${await this.getToken()}` },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  getToken = async () =>
+    (await Auth.currentSession()).getIdToken().getJwtToken();
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+        <AmplifySignOut />
+          <HeaderMenu />
+
+          <Route path="/" exact render={() => <OrderList />} />
+
+          <Route path="/my-orders" exact render={() => <MyOrders />} />
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default withAuthenticator(App);
